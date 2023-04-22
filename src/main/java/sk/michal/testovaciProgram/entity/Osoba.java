91b3b6824971cf0dec347b8335c30562cc9f368a
Michal Pavlovsky <pavlovsky.miso@gmail.com>
2023-04-22 23:42:45 +0200

    Basic

---
+++ src/main/java/sk/michal/testovaciProgram/entity/Osoba.java
@@ -0,0 +1,12 @@
+package sk.michal.testovaciProgram.entity;
+
+import sk.michal.ormSimpleFramework.anotacie.Tabulka;
+
+@Tabulka("OSOBA")
+public class Osoba {
+    @Id
+    @Stlpec
+    private Long id;
+    @Stlpec
+
+}
