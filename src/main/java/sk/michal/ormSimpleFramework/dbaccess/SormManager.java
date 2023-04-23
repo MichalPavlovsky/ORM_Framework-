60e49e907dabd38b8b394ab705f9b2da4621001b
Michal Pavlovsky <pavlovsky.miso@gmail.com>
2023-04-23 21:56:33 +0200

    Add osoba, anotations

---
+++ src/main/java/sk/michal/ormSimpleFramework/dbaccess/SormManager.java
@@ -0,0 +1,5 @@
+package sk.michal.ormSimpleFramework.dbaccess;
+
+public class SormManager {
+
+}
--- src/main/java/sk/michal/testovaciProgram/entity/Osoba.java
+++ src/main/java/sk/michal/testovaciProgram/entity/Osoba.java
@@ -1,12 +1,25 @@
 package sk.michal.testovaciProgram.entity;
 
+import sk.michal.ormSimpleFramework.anotacie.Id;
+import sk.michal.ormSimpleFramework.anotacie.Stlpec;
 import sk.michal.ormSimpleFramework.anotacie.Tabulka;
 
 @Tabulka("OSOBA")
 public class Osoba {
     @Id
-    @Stlpec
+    @Stlpec("ID")
     private Long id;
-    @Stlpec
+
+    @Stlpec("NAME")
+    private String meno;
+
+    @Stlpec("SURNAME")
+    private String priezvisko;
+
+    @Stlpec("AGE")
+    private int vek;
+
+    @Stlpec("ADDRESS")
+    private String adresa;
 
 }
